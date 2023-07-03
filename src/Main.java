905794265ecf5730324e2e5b407a25a0dc0e3c26
DmitryNazarov <dima-nazarov-98@list.ru>
2023-07-03 11:31:44 +0300

    3 Финальный проект после второго ревью!

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import tasks.Task;
 
 public class Main {
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        /*TaskManager taskManager = new TaskManager();
 
         Task task1 = new Task("Задача 1", "Опиание задчачи");
         Task task2 = new Task("Задача 2", "Опиание задчачи");
@@ -133,6 +133,58 @@ public class Main {
         Subtask subtask8 = new Subtask("Задача 5", "Опиание задчачи Epic1", epic1.getId());
         subtask8 = taskManager.createSubtask(subtask8);
         taskManager.updateEpicAndSubtask(epic1);
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");*/
+
+        TaskManager taskManager = new TaskManager();
+        Task task1 = new Task("Задача 1", "Опиание задчачи");
+        Task task2 = new Task("Задача 2", "Опиание задчачи");
+
+        Epic epic1 = new Epic("Задача 1", "Опиание задчачи Epic1");
+        Epic epic2 = new Epic("Задача 2", "Опиание задчачи Epic2");
+
+        task1 = taskManager.createTask(task1);
+        task1.setName("Задача 1.2");
+
+        task2 = taskManager.createTask(task2);
+
+        epic1 = taskManager.createEpic(epic1);
+        epic2 = taskManager.createEpic(epic2);
+
+        Subtask subtask1 = new Subtask("Задача 1", "Опиание задчачи Epic1", epic1.getId());
+        Subtask subtask2 = new Subtask("Задача 2", "Опиание задчачи Epic1", epic1.getId());
+        Subtask subtask3 = new Subtask("Задача 3", "Опиание задчачи Epic1", epic1.getId());
+        Subtask subtask4 = new Subtask("Задача 4", "Опиание задчачи Epic1", epic1.getId());
+        Subtask subtask5 = new Subtask("Задача 5", "Опиание задчачи Epic1", epic1.getId());
+        Subtask subtask6 = new Subtask("Задача 1", "Опиание задчачи Epic2", epic2.getId());
+
+        subtask1 = taskManager.createSubtask(subtask1);
+        subtask2 = taskManager.createSubtask(subtask2);
+        subtask3 = taskManager.createSubtask(subtask3);
+        subtask4 = taskManager.createSubtask(subtask4);
+        subtask5 = taskManager.createSubtask(subtask5);
+        subtask6 = taskManager.createSubtask(subtask6);
+
+
+        taskManager.updateEpicAndSubtask(epic1);
         System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        subtask3.setStatus(managers.Status.DONE);
+
+        taskManager.updateEpicAndSubtask(epic1);
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        subtask1.setStatus(Status.DONE);
+        subtask2.setStatus(Status.DONE);
+        subtask3.setStatus(Status.IN_PROGRESS);
+        subtask4.setStatus(Status.DONE);
+        subtask5.setStatus(Status.DONE);
+        subtask6.setStatus(Status.DONE);
+        taskManager.updateEpicAndSubtask(epic1);
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        taskManager.deleteSubtaskByID(subtask3.getId());
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        taskManager.updateEpicAndSubtask(epic1);
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        taskManager.deleteEpicByID(epic1.getId());
+        System.out.println("\n" + taskManager.getAllEpic() + "\n");
+        System.out.println("\n" + taskManager.getAllSubtask() + "\n");
     }
 }
